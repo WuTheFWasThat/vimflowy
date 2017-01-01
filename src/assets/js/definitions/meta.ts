e25e0017b0caabfa837d29ebb893ad4ca161d3b2
Jeff Wu <wuthefwasthat@gmail.com>
2016-12-31 19:41:59 -0800

    fix exportFile

--- src/assets/js/definitions/meta.ts
+++ src/assets/js/definitions/meta.ts
@@ -3,8 +3,8 @@ import keyDefinitions, { Action, SequenceAction } from '../keyDefinitions';
 keyDefinitions.registerAction(new Action(
   'export-file',
   'Export as Json file',
-  async function() {
-    await this.session.exportFile('json');
+  async function({ session }) {
+    await session.exportFile('json');
   },
   {
     sequence: SequenceAction.DROP,
