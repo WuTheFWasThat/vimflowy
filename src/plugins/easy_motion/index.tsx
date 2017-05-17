a2f9f43a024ed27e068d0fd3b14136bf5f204070
Jeff Wu <wuthefwasthat@gmail.com>
2017-05-16 22:41:10 -0700

    fix easy motion

--- src/plugins/easy_motion/index.tsx
+++ src/plugins/easy_motion/index.tsx
@@ -1,5 +1,6 @@
 import * as _ from 'lodash';
 import * as React from 'react'; // tslint:disable-line no-unused-variable
+import * as $ from 'jquery';
 
 import Path from '../../assets/ts/path';
 import { registerPlugin } from '../../assets/ts/plugins';
