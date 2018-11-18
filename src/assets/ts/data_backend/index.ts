b3a9726894ce4a16e6b876a9b3f813d768b67a8f
Jeff Wu <wuthefwasthat@gmail.com>
2018-11-18 01:17:25 -0800

    undo firebase import change

--- src/assets/ts/data_backend/index.ts
+++ src/assets/ts/data_backend/index.ts
@@ -1,4 +1,4 @@
-import * as firebase from 'firebase/app';
+import * as firebase from 'firebase';
 
 import EventEmitter from '../utils/eventEmitter';
 import DataBackend, { SynchronousDataBackend } from '../../../shared/data_backend';
