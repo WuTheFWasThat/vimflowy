202fcee14e566c482cdd6f90bed3cf018934822f
Victor Tao <platers81@gmail.com>
2021-01-21 12:07:59 -0500

    small cleanup

--- src/plugins/index.ts
+++ src/plugins/index.ts
@@ -11,6 +11,8 @@ import './deadlines';
 import './tags';
 
 // for developers: uncomment the following lines
+/*
 import './debug_mode';
 import './examples';
+*/
 // TODO: make this automatically happen in webpack dev?
