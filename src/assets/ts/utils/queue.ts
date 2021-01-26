462c8bf7e5879fb02add631d08b6e8786fdd5e54
Jeff Wu <wuthefwasthat@gmail.com>
2021-01-25 17:19:04 -0800

    for some reason ExtendableError name doesnt work for unit tests

--- src/assets/ts/utils/queue.ts
+++ src/assets/ts/utils/queue.ts
@@ -1,8 +1,8 @@
 import { ExtendableError } from '../../../shared/utils/errors';
 
 export class QueueStoppedError extends ExtendableError {
-  constructor(m = '') {
-    super(m ? `Queue stopped: ${m}!` : 'Queue stopped!');
+  constructor(m = '', name='QueueStoppedError') {
+    super(m ? `Queue stopped: ${m}!` : 'Queue stopped!', name);
   }
 }
 
